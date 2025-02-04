b3a2526849b8e1772d33b563fb833cbef608e84d
scotcham <scotcham@hotmail.com>
2025-02-03 18:42:58 -0600

    init commit

--- test2.ps1
+++ test2.ps1
@@ -1,1 +1,11 @@
-test
+# Get OS information using Get-CimInstance
+$osInfo = Get-CimInstance Win32_OperatingSystem
+
+# Display relevant OS details
+Write-Output "Operating System: $($osInfo.Caption)"
+Write-Output "Version: $($osInfo.Version)"
+Write-Output "Build Number: $($osInfo.BuildNumber)"
+Write-Output "Architecture: $($osInfo.OSArchitecture)"
+Write-Output "Installed On: $($osInfo.InstallDate)"
+Write-Output "Last Boot Time: $($osInfo.LastBootUpTime)"
+Write-Output "System Directory: $($osInfo.SystemDirectory)"
